30aaf4fea54382672c6624a1e8690ad90bd8f972
Somesh Kumar <87225934+kumarsomeshunos@users.noreply.github.com>
2022-03-30 15:18:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+//incase we need to throw an error
+import ExpressErrorHandler from "./middlewares/ErrorHandlers/ExpressErrorHandler.js";
+
 import authRouter from "./routes/auth.js";
 
 mongoose
@@ -31,6 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+        const { status = 500, message = 'Something went wrong!' } = err;
+        res.status(status).send(message);
+})
 
 app.listen(parseInt(process.env.PORT), () =>
 	console.log("API listening on port " + process.env.PORT)
