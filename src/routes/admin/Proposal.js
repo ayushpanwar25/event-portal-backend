b03d01b52de23cd3c89f3312fceef511631efe65
Somesh Kumar <87225934+kumarsomeshunos@users.noreply.github.com>
2022-04-02 16:12:44 +0530

    Add crud functionality

--- src/routes/admin/Proposal.js
+++ src/routes/admin/Proposal.js
@@ -5,16 +5,44 @@ import { verifyAdmin } from "../../middlewares/checkAuth";
 const router = express.Router();
 
 router.get("/get", verifyAdmin, async (req, res) => {
-
+    const proposals = await Proposal.find({});
+    res.send(proposals);
 });
 
 router.get("/get/:id", verifyAdmin, async (req, res) => {
-	
+	const _id = req.params.id;
+    const proposal = await Proposal.findById({ _id });
+    res.send(proposal);
 });
 
+router.get('/newproposal', verifyAdmin, async (req, res) => {
+    res.send('New proposal input field');
+})
 
-router.delete("/delete/:id", verifyAdmin, async (req, res) => {
+router.post('/newproposal', verifyAdmin, (req, res) => {
+    const newProposal = new Proposal(req.body);
+    await newProposal.save();
+    res.send('Saved')
+})
 
+router.get('/update/:id', verifyAdmin, async(req, res) => {
+    const _id = req.params.id;
+    const proposal = await Proposal.findById({ _id });
+    res.send(proposal);
+})
+
+router.post('/update/:id', verifyAdmin, async(req, res) => {
+    const _id = req.params.id;
+    const proposal = await Proposal.findByIdAndUpdate( _id, req.body );
+    await proposal.save();
+    res.send('Updated');
+})
+
+
+router.post("/delete/:id", verifyAdmin, async (req, res) => {
+    const _id = req.params.id;
+    await Proposal.findByIdAndDelete({ _id });
+    res.send('Deleted');
 });
 
 export default router;
