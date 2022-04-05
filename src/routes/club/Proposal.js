4c8f71d2f73f6bf83e28286738de62ed2c873187
Somesh Kumar <87225934+kumarsomeshunos@users.noreply.github.com>
2022-04-05 15:28:59 +0530

    Update Proposal.js

--- src/routes/club/Proposal.js
+++ src/routes/club/Proposal.js
@@ -28,15 +28,49 @@ router.get("/get/:id", verifyClub, async (req, res) => {
 
 
 router.post("/create", verifyClub, async (req, res) => {
-
+	new Proposal(req.body)
+		.then( async (newProposal) => {
+			if(!newProposal) {
+				return res.status(500).send({ success: false, message: "Something went wrong" });
+			}
+			if(newProposal.clubId.equals(req.session.userId)) {
+				await newProposal.save();
+				return res.json({ success: true, newProposal });
+			}
+			else {
+				return res.status(403).send({ success: false, message: "Forbidden" });
+			}
+		})
 });
 
 router.put("/edit/:id", verifyClub, async (req, res) => {
-
+	Proposal.findByIdAndUpdate(req.params.id, req.body)
+		.then((proposal) => {
+				if(!proposal) {
+					return res.status(500).send({ success: false, message: "Proposal not found" });
+				}
+				if(proposal.clubId.equals(req.session.userId)) {
+					return res.json({ success: true, message: "Proposal Updated" });
+				}
+				else {
+					return res.status(403).send({ success: false, message: "Forbidden" });
+				}
+		});
 });
 
 router.delete("/delete/:id", verifyClub, async (req, res) => {
-
+	Proposal.findByIdAndDelete(req.params.id)
+		.then((proposal) => {
+			if(!proposal) {
+				return res.status(500).send({ success: false, message: "Proposal not found" });
+			}
+			if(proposal.clubId.equals(req.session.userId)) {
+				return res.json({ success: true, message: "Proposal Deleted" });
+			}
+			else {
+				return res.status(403).send({ success: false, message: "Forbidden" });
+			}
+		});
 });
 
 export default router;
