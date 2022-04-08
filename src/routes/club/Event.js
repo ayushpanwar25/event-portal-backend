a4de592236c07dc0ae8499dce4cb0c4b29abb021
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2022-04-08 13:09:31 +0530

    club event routes improvement and added delete route

--- src/routes/club/Event.js
+++ src/routes/club/Event.js
@@ -9,18 +9,14 @@ import { verifyClub } from "../../middlewares/checkAuth.js";
 
 const router = express.Router();
 
-
-// TODO: Route to change visibility of event after approval == 3
-// TODO: Route to delete event required? Since we can't delete proposal for sake of keeping info, we can add cancelled attribute
-
 router.get("/", verifyClub, async (req, res) => {
-    Event.find({clubId: req.session.userId})
-    .select('club clubId title description date')
+    Event.find({ clubId: req.session.userId })
+        .select('club clubId title description date')
         .then(eventsList => {
-            res.status(200).json({success: true, events: eventsList})
+            res.status(200).json({ success: true, events: eventsList })
         })
         .catch(err => {
-            res.status(500).send({success: false, message: "Failed to query database", error: err})
+            res.status(500).send({ success: false, message: "Failed to query database", error: err })
         })
 });
 
@@ -32,49 +28,91 @@ router.get("/:id", verifyClub, async (req, res) => {
         .where('clubId').equals(req.session.userId)
         .select('club clubId title description date visible')
         .then(event => {
-            if (event){
-                res.status(200).json({success: true, event: event})
+            if (event) {
+                res.status(200).json({ success: true, event: event })
             }
-            else{
-                res.status(404).send({success: false, message: "Not found"})
+            else {
+                res.status(404).send({ success: false, message: "Not found" })
             }
         })
         .catch(err => {
-            res.status(500).send({success: false, message: "Failed to query database", error: err})
+            res.status(500).send({ success: false, message: "Failed to query database", error: err })
         })
 });
 
 
 router.put("/:id", verifyClub, async (req, res) => {
-    // TODO: At what stage and what fields will club be allowed to edit
-	Event.findById(req.params.id)
-		.then(event => {
-			if (event.clubId == req.session.userId) {
-                
+    Event.findById(req.params.id)
+        .then(async (event) => {
+            if (event.clubId == req.session.userId) {
+
+                // Verify approval stage for visible field
+                if (req.body.visible == true){
+                    try{
+                        let proposal = await Proposal.findById(event.proposalId)
+                        if (proposal.approval !== 3){
+                            return res.status(400).send({success: false, message: "Proposal not approved"})
+                        }
+                    }
+                    catch (err) {
+                        res.status(500).send({success: false, message: "Failed to find proposal"})
+                    }
+                }
+
                 // Can be improved, possibly move updatablefield validation to model
                 const updatableFields = ["title", "description", "visible", "date"]
-                updatableFields.forEach(function(item){
-                    if (req.body[item] !== undefined){
+                updatableFields.forEach(function (item) {
+                    if (req.body[item] !== undefined) {
                         event[item] = req.body[item]
                     }
                 })
 
                 event.save()
-					.then(eventUpdated => {
-						res.status(200).send({ success: true, id: eventUpdated.id })
-					})
-					.catch(err => {
-						res.status(500).send({success: false, message: "Failed to update proposal", error: err})
-					})
-			}
-			else {
-				res.status(403).send({ success: false, message: "You do not have access to this event" })
-			}
-		})
-		.catch((err) => {
-			res.status(404).send({ success: false, message: "Event not found", error: err })
-		})
+                    .then(eventUpdated => {
+                        res.status(200).send({ success: true, id: eventUpdated.id })
+                    })
+                    .catch(err => {
+                        res.status(500).send({ success: false, message: "Failed to update proposal", error: err })
+                    })
+            }
+            else {
+                res.status(403).send({ success: false, message: "You do not have access to this event" })
+            }
+        })
+        .catch((err) => {
+            res.status(404).send({ success: false, message: "Event not found", error: err })
+        })
 
 });
+
+
+// Delete event by id
+router.delete("/:id", verifyClub, async (req, res) => {
+    // TODO: Several issues with letting club deleet event
+    // TODO: Should approval be automatically set to -1 if event deleted?
+    // TODO: Does one proposal allow having multiple event?
+    Event.findById(req.params.id)
+        .where('clubId').equals(req.session.userId)
+        .then(event => {
+            if (event) {
+                event.delete()
+                .then(() => {
+                    res.status(200).send({ success: true })
+                })
+                .catch(err => {
+                    res.status(500).send({success: false, error: err})
+                })
+            }
+            else {
+                res.status(404).send({ success: false, message: "Not found" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ success: false, message: "Failed to query database", error: err })
+        })
+});
+
+
+
 
 export default router;
